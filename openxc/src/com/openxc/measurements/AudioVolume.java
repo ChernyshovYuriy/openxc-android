cba9bbabd2404d94eb4d3df1b0236c52734342ad
YuriyChernyshov <IChernyshov@luxoft.com>
2013-11-28 12:03:24 +0200

    Add new measurement

---
+++ openxc/src/com/openxc/measurements/AudioVolume.java
@@ -0,0 +1,11 @@
+package com.openxc.measurements;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: user
+ * Date: 11/28/13
+ * Time: 11:39 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AudioVolume {
+}
